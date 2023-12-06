a028ac4373bc0d653db07e923b73125396fd2256
juan <osunajuan15@gmail.com>
2023-12-06 13:36:03 -0300

    arreglo m-top

--- src/COMPONENTS/Layout/index.jsx
+++ src/COMPONENTS/Layout/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 function Layout() {
   return (
-    <div className='flex flex-col flex-grow items-center mt-[15%]  md:mt-[9%] lg:mt-[6%] mb-16 w-11/12 md:w-4/5 '>
+    <div className='flex flex-col flex-grow items-center mt-[20%]  md:mt-[9%] lg:mt-[6%] mb-16 w-11/12 md:w-4/5 '>
         <Outlet/>
     </div>
   )
